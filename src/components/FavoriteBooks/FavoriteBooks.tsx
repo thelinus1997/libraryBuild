2039d5452ec6b8de34d072eb4f94e5febe4a510b
thelinus1997 <thelinus1997@gmail.com>
2024-05-13 09:28:46 +0200

    basic favorite book page created

--- src/components/FavoriteBooks/FavoriteBooks.tsx
+++ src/components/FavoriteBooks/FavoriteBooks.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getFavoriteBooks } from "../../Slices/favBookSlice";
+import { Doc } from "../../types/titleType";
+import ResultCard from "../SearchContainer/ResultContainer/ResultCard/ResultCard";
+
+interface RetrievedBooks {
+  books: [];
+}
 
 const FavoriteBooks = () => {
-  return <div>FavoriteBooks</div>;
+  const [favoriteBooks, setFavoriteBooks] = useState<RetrievedBooks>();
+  const retrievedFavBookData = useSelector(
+    getFavoriteBooks
+  ) as unknown as RetrievedBooks;
+  useEffect(() => {
+    console.log(retrievedFavBookData.books);
+    setFavoriteBooks(retrievedFavBookData);
+  }, [retrievedFavBookData]);
+
+  return (
+    <div>
+      <div className="flex">
+        {favoriteBooks &&
+          favoriteBooks.books.map((item, index) => (
+            <li key={index} className="flex">
+              <ResultCard item={item} />
+            </li>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default FavoriteBooks;
