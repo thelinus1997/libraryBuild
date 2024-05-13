fff9a8318da43789d51d3c5a399257e29f80d19d
Linus Orrekvist <thelinus1997gmail.com>
2024-05-13 14:32:36 +0200

    pages read basics added

--- src/components/SpecificBook/SpecificBook.tsx
+++ src/components/SpecificBook/SpecificBook.tsx
@@ -5,13 +5,21 @@ import { Doc } from "../../types/titleType";
 const SpecificBook = () => {
   const [book, setBook] = useState<Doc>();
   const [score, setScore] = useState(1);
+  const [pagesRead, setPagesRead] = useState(0);
   let { state } = useLocation();
   useEffect(() => {
     setBook(state.value);
   }, [state]);
-  console.log(state.value);
+
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setScore(parseInt(e.target.value));
+  };
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (parseInt(e.target.value)) {
+      setPagesRead(parseInt(e.target.value));
+    } else {
+      setPagesRead(0);
+    }
   };
   return (
     <div className="flex justify-center my-10 ">
@@ -30,21 +38,38 @@ const SpecificBook = () => {
               Published: {book.first_publish_year}
             </p>
             <p className="m-3 text-center">Author(s): {book.author_name}</p>
-            <p className="m-3 text-center">
-              {book.first_sentence && book.first_sentence[2]}
-            </p>
+            <div className="m-3 text-center">
+              {book.first_sentence &&
+                book.first_sentence.map((sentance, index) => (
+                  <p className="my-3" key={index}>
+                    {sentance}
+                  </p>
+                ))}
+            </div>
           </div>
           <div className="flex-col w-6/12 justify-center">
             <p className="mx-3 text-center">
               Number of people who have read this book:{" "}
               {book.already_read_count}
             </p>
-            <div className="mx-3 text-center">
-              Have you read this book? Leave a review!
+            <div className="mx-3 my-10 text-center">
+              How many pages have you read?
+              <div className="flex flex-row justify-center">
+                <input
+                  className="bg-white border border-indigo-600  h-full rounded p-0 mx-1 w-2/12"
+                  type="text"
+                  onChange={handleInput}
+                  value={pagesRead}
+                />
+                <p>Out of: {book.number_of_pages_median} pages.</p>
+              </div>
             </div>
-            <div className="mx-3 text-center h-10">
+            <div className="mx-3 text-center">
+              Have you finished reading this book? Leave a review!
+            </div>
+            <div className="mx-3 text-center h-6">
               <select
-                className="bg-white border border-indigo-600  h-full rounded p-2 mx-1"
+                className="bg-white border border-indigo-600  h-full rounded p-0 mx-1"
                 value={score}
                 onChange={handleSelectChange}
               >
@@ -54,7 +79,7 @@ const SpecificBook = () => {
                 <option value={4}>4</option>
                 <option value={5}>5</option>
               </select>
-              <button className="bg-white border border-indigo-600 h-full rounded p-2">
+              <button className="bg-white border border-indigo-600 h-full rounded p-0">
                 Submit score
               </button>
             </div>
